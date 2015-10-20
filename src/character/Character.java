4c42afefa582b73c3203cfff55e4bf4e71d376c5
kbuckss <buckley_kd@yahoo.com>
2015-10-20 12:51:32 -0700

    Initial check-in

---
+++ src/character/Character.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package character;
+
+import environment.ApplicationStarter;
+
+/**
+ *
+ * @author Katherine
+ */
+public class Character {
+
+    public static void main(String[] args) {
+        ApplicationStarter.run("Patrick Starr", new CharacterEnvironment());
+    }
+    
+}
